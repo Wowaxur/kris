39d8100d730751e73bb3bbe4714457570cfde012
Wowaxur <wowaxur@yandex.ru>
2024-01-15 14:24:12 +0300

    innit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import MainImg from './components/images/mainImg.webp'
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import SvoiLoogo from './components/images/logo_2.png'
 import VkLogo from './components/images/vk.svg'
 class App extends React.Component {
     render() {
@@ -13,30 +14,48 @@ class App extends React.Component {
             <div className={'Image'}>
             <img className={'MainImage'} src={MainImg}/>     </div>
                 <div className={"Name"}>
-                    <h3>KRISTINA</h3>
+                    <h3>Кристина Дончевская</h3>
+                    <h5>Руководитель партнерского направления
+                        Г. Краснодар
+                    </h5>
+
                 </div>
                 <BlockItem/>
                 <div className={'ContainerBlock'}>
+                    <div className={'WhatsappWrapper'}>
                 <div className={'BlockContact'}>
                     <a target={"_blank"} href={'https://wa.me/79180892823'} className={"WhatsApp"}>
                         <WhatsAppIcon className="icon" />
-                        <div>WhatsApp</div>
+                        <div>Рабочий</div>
                     </a>
                 </div>
                     <div className={'BlockContact'}>
-                    <a  href={'http://t.me/Chris_Donch'} target={"_blank"} className={"Telegram"}>
+                        <a target={"_blank"} href={'https://wa.me/79180892823'} className={"WhatsApp"}>
+                            <WhatsAppIcon className="icon" />
+                            <div>Личный</div>
+                        </a>
+                    </div>
+                    </div>
+                    <div className={'BlockContact'}>
+                    <a  href={'https://t.me/+4YItH4K3uuk5NTk6'} target={"_blank"} className={"Telegram"}>
                         <TelegramIcon className={'icon'}/>
                         Telegram</a>
                 </div>
                     <div className={'BlockContact'}>
-                    <a href={'https://www.instagram.com/kristina_donch/'} target={"_blank"} className={"Instagram"}>
+                    <a href={'https://www.instagram.com/svoi.live/'} target={"_blank"} className={"Instagram"}>
                         <InstagramIcon className={'icon'}/>
                         Instagram</a>
                 </div>
                     <div className={'BlockContact'}>
-                    <a href={'https://vk.com/kristinka_donch'} className={"Link"} target={"_blank"}> <img src={VkLogo} className={'vkLogo'}/> </a>
+                    <a href={'https://vk.com/svoi.live'} className={"Link"} target={"_blank"}> <img src={VkLogo} className={'vkLogo'}/> </a>
                 </div>
+                    <div className={'BlockContact'}>
+                        <a href={'https://svoi.live'} className={"Link"} target={"_blank"}> <img className={'SvoiLogo'} src={SvoiLoogo} />
+                      </a>
+                    </div>
+
                 </div>
+
             </div>
         );
     }
